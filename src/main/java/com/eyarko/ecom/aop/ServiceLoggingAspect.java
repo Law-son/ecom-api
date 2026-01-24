55afd46f5fbbe7b78c4d770d4b912699a938f3ee
Law-son <buabassahlawson@gmail.com>
2026-01-24 17:03:00 +0000

    feat: add service before and after logs

--- src/main/java/com/eyarko/ecom/aop/ServiceLoggingAspect.java
+++ src/main/java/com/eyarko/ecom/aop/ServiceLoggingAspect.java
@@ -1,8 +1,11 @@
 package com.eyarko.ecom.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -11,8 +14,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class ServiceLoggingAspect {
     private static final Logger logger = LoggerFactory.getLogger(ServiceLoggingAspect.class);
+    private static final String SERVICE_POINTCUT = "execution(* com.eyarko.ecom.service..*(..))";
 
-    @Around("execution(* com.eyarko.ecom.service..*(..))")
+    @Before(SERVICE_POINTCUT)
+    public void logServiceStart(JoinPoint joinPoint) {
+        logger.debug("Starting {}", joinPoint.getSignature());
+    }
+
+    @After(SERVICE_POINTCUT)
+    public void logServiceEnd(JoinPoint joinPoint) {
+        logger.debug("Finished {}", joinPoint.getSignature());
+    }
+
+    @Around(SERVICE_POINTCUT)
     public Object logServiceExecution(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         try {
