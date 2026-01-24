628c904560c22ae5291889ab84595949c0806c8c
Law-son <buabassahlawson@gmail.com>
2026-01-24 19:37:37 +0000

    chore: remove unused order item repository

--- src/main/java/com/eyarko/ecom/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eyarko.ecom.repository;
-
-import com.eyarko.ecom.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
-
-
